0193651b75d7217d01e767cb80d55b0a07d5d437
Rujan shakya <rujanshakya76@gmail.com>
2017-06-08 18:07:41 +0000

    INitital Commit

--- app/controllers/foos_controller.rb
+++ app/controllers/foos_controller.rb
@@ -9,6 +9,11 @@ class FoosController < ApplicationController
       @source = 'https://api.chucknorris.io/jokes/random'
       response = Net::HTTP.get_response(URI.parse(@source))
       @json = JSON.parse(response.body)
+      respond_to do |format|
+        format.html { render  html: "" }
+        format.json { render json: @json }
+        format.xml { render xml: @json }
+      end
     end
     
 end
