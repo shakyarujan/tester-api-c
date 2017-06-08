289eeeee8cf3377b0d6c7a888cb8ec8b73686942
Rujan shakya <rujanshakya76@gmail.com>
2017-06-08 12:48:18 +0000

    Final Touch

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,6 @@
 /* global $ */
-
+/* global localStorage */
+/* global $window */
 // $(function(){
 //   $.ajax({
 //     url: 'https://api.chucknorris.io/jokes/random?',
@@ -10,13 +11,10 @@
 //   });
 // })
 
-
-
 // ---------About - pages ------------
-
 $(document).ready(function(){
     $('#username').on('keyup',function(e){
-      var username = e.target.val;
+      var username = e.target.value;
       
        //Make request to github
        
@@ -27,7 +25,7 @@ $(document).ready(function(){
                client_secret:'dd99328326434dba2883ad9e31fc13127578545e'
            }
        }).done(function(user){
-     $.ajax({
+      $.ajax({
         url:'https://api.github.com/users/'+username+'/repos',
         data:{
           client_id:'da642b344f47401ba760',
@@ -55,54 +53,42 @@ $(document).ready(function(){
             </div>
           `);
         });
-    });
+      });
                     
-    $('#profile').html(`
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="panel-title">${user.name}</h3>
-            </div>
-            <div class="panel-body">
-               <div class="row">
-               <div class="col-md-3">
-                <img class="thumbnail avatar" src="${user.avatar_url}">
-                <a href="${user.html_url}" class="btn btn-primary btn-block" target="_blank">View Profile</a>
-            </div>
-                <div class="col-md-9">
-                    <span class="label label-default">Repo:${user.public_repos}</span>
-                    <span class="label label-primary">Gist:${user.public_gists}</span>
-                    <span class="label label-success">Followers:${user.followers}</span>
-                    <span class="label label-info">Following:${user.following}</span>
-                <br><br>
-                <ul class ="list-group">
-                    <li class="list-group-item">Company:${user.company}</li>
-                    <li class="list-group-item">Website/Blog: ${user.blog}</li>
-                    <li class="list-group-item">Location: ${user.location}</li>
-                    <li class="list-group-item">Member Since: ${user.created_at}</li>
-                </ul>
+           $('#profile').html(`
+                       <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">${user.name}</h3>
                 </div>
-               </div>
-            </div>
-        </div>
+                <div class="panel-body">
+                   <div class="row">
+                   <div class="col-md-3">
+                    <img class="thumbnail avatar" src="${user.avatar_url}">
+                    <a href="${user.html_url}" class="btn btn-primary btn-block" target="_blank">View Profile</a>
+                </div>
+                    <div class="col-md-9">
+                        <span class="label label-default">Repo:${user.public_repos}</span>
+                        <span class="label label-primary">Gist:${user.public_gists}</span>
+                        <span class="label label-success">Followers:${user.followers}</span>
+                        <span class="label label-info">Following:${user.following}</span>
+                    <br><br>
+                    <ul class ="list-group">
+                        <li class="list-group-item">Company:${user.company}</li>
+                        <li class="list-group-item">Website/Blog: ${user.blog}</li>
+                        <li class="list-group-item">Location: ${user.location}</li>
+                        <li class="list-group-item">Member Since: ${user.created_at}</li>
+                    </ul>
+                    </div>
+                   </div>
+                </div>
+         </div>
             <h3 class="page-header">Latest Repos</h3>
-        <div id="repos"></div>
+            <div id="repos"></div>
            `);
        });
     });
    
     
 });
-
 // <---end---->
-
-$(document).ready(function (){
-    $('#username').keyup(function(event) {
-        // 27 is key code of ESC
-        if (event.keyCode == 27) {
-            $('#username').val('shakyarujan');
-            // Loose focus on input field
-            $('#username').blur();
-        }
-    });
-});
 
